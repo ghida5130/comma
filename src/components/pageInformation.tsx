2f4a0308d4904511a79a7d0ed593ab71260141c2
unknown <ghida5130@gmail.com>
2025-02-02 02:33:25 +0900

    깃허브 버튼, 링크 추가

--- src/components/pageInformation.tsx
+++ src/components/pageInformation.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import styles from "../styles/pageInformation.module.css";
 import Image from "next/image";
-import reactIcon from "@/public/icons/react2.png";
-import nextIcon from "@/public/icons/next3.png";
+import reactIcon from "@/public/icons/react.png";
+import nextIcon from "@/public/icons/next.png";
+import githubIcon from "@/public/icons/github.png";
 
 export default function PageInformation({
     data,
 }: {
-    data: { title: string; framework: string; description: string; link: string };
+    data: { title: string; framework: string; description: string; link: string; githubLink: string };
 }) {
     let icon = data.framework === "React" ? reactIcon : nextIcon;
     return (
@@ -16,7 +17,12 @@ export default function PageInformation({
                 {data.title} ›
             </Link>
             <p className={styles.description}>{data.description}</p>
-            <Image className={styles.icon} src={icon} alt={`${data.framework} icon`} height={30} />
+            <div className={styles.iconArea}>
+                <Image className={styles.icon} src={icon} alt={`${data.title} ${data.framework} icon`} height={30} />
+                <Link href={data.githubLink}>
+                    <Image className={styles.icon} src={githubIcon} alt={`${data.title} github icon`} height={30} />
+                </Link>
+            </div>
         </div>
     );
 }
--- src/data/pageData.ts
+++ src/data/pageData.ts
@@ -4,18 +4,21 @@ const pageData = [
         framework: "Next",
         description: "영화 정보 조회 홈페이지",
         link: "https://millivie.vercel.app/",
+        githubLink: "https://github.com/ghida5130/millivie",
     },
     {
         title: "MilliUlsan",
         framework: "React",
         description: "울산광역시 정보 조회 홈페이지",
         link: "https://milli-ulsan.vercel.app/",
+        githubLink: "https://github.com/ghida5130/milliUlsan",
     },
     {
         title: "Meλeti",
         framework: "Next",
         description: "독서기록 모바일 서재",
         link: "https://meleti-sigma.vercel.app/",
+        githubLink: "https://github.com/ghida5130/meleti",
     },
 ];
 

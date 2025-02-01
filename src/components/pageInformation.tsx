d667d163e659cc4e8c156e75425d5f28773e087d
unknown <ghida5130@gmail.com>
2025-02-02 01:28:52 +0900

    commit

---
+++ src/components/pageInformation.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/pageInformation.module.css";
+
+export default function PageInformation() {
+    return (
+        <div className={styles.pageInfo}>
+            <h1>title</h1>
+            <h2>사용 기술들 나열</h2>
+        </div>
+    );
+}

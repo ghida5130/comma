d37da99c4e2e0207f70108888ac450b4cd78f4ef
unknown <ghida5130@gmail.com>
2025-02-04 22:29:40 +0900

    호환 아이콘 추가, 아이콘 분리

--- src/components/pageInformation.tsx
+++ src/components/pageInformation.tsx
@@ -1,16 +1,23 @@
 import Link from "next/link";
 import styles from "../styles/pageInformation.module.css";
 import Image from "next/image";
-import reactIcon from "@/public/icons/react.png";
+import reactIcon from "@/public/icons/react.svg";
 import nextIcon from "@/public/icons/next.png";
-import githubIcon from "@/public/icons/github.png";
+import githubIcon from "@/public/icons/github.svg";
+
+import { StaticImageData } from "next/image";
 
 export default function PageInformation({
     data,
 }: {
-    data: { title: string; framework: string; description: string; link: string; githubLink: string };
+    data: {
+        title: string;
+        description: string;
+        link: string;
+        githubLink: string;
+        icons: StaticImageData[];
+    };
 }) {
-    let icon = data.framework === "React" ? reactIcon : nextIcon;
     return (
         <div className={styles.pageInfo}>
             <Link className={styles.title} href={data.link}>
@@ -18,7 +25,18 @@ export default function PageInformation({
             </Link>
             <p className={styles.description}>{data.description}</p>
             <div className={styles.iconArea}>
-                <Image className={styles.icon} src={icon} alt={`${data.title} ${data.framework} icon`} height={30} />
+                {data.icons.map((src, idx) => {
+                    if (idx === 1) {
+                        return (
+                            <>
+                                <Divide />
+                                <Image src={src} alt={`${data.title} icons`} height={30} />
+                            </>
+                        );
+                    }
+                    return <Image src={src} alt={`${data.title} icons`} height={30} />;
+                })}
+                <Divide />
                 <Link href={data.githubLink}>
                     <Image className={styles.icon} src={githubIcon} alt={`${data.title} github icon`} height={30} />
                 </Link>
@@ -26,3 +44,7 @@ export default function PageInformation({
         </div>
     );
 }
+
+const Divide = () => {
+    return <div className={styles.divideLine}></div>;
+};
--- src/data/pageData.ts
+++ src/data/pageData.ts
@@ -1,24 +1,29 @@
+import reactIcon from "@/public/icons/react.png";
+import nextIcon from "@/public/icons/next.png";
+import pcIcon from "@/public/icons/pc.svg";
+import mobileIcon from "@/public/icons/mobile.svg";
+
 const pageData = [
     {
         title: "Millivie",
-        framework: "Next",
         description: "영화 정보 조회 홈페이지",
         link: "https://millivie.vercel.app/",
         githubLink: "https://github.com/ghida5130/millivie",
+        icons: [nextIcon, pcIcon],
     },
     {
         title: "MilliUlsan",
-        framework: "React",
         description: "울산광역시 정보 조회 홈페이지",
         link: "https://milli-ulsan.vercel.app/",
         githubLink: "https://github.com/ghida5130/milliUlsan",
+        icons: [reactIcon, pcIcon, mobileIcon],
     },
     {
         title: "Meλeti",
-        framework: "Next",
         description: "독서기록 모바일 서재",
         link: "https://meleti-sigma.vercel.app/",
         githubLink: "https://github.com/ghida5130/meleti",
+        icons: [nextIcon, mobileIcon],
     },
 ];
 

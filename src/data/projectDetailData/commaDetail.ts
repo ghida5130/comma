973d843f82ad610b3d52f315dbd5953cfebeb4c7
ghida5130 <ghida5130@gmail.com>
2025-08-08 21:03:51 +0900

    chore: 기타 주석 정리

--- src/data/projectDetailData/commaDetail.ts
+++ src/data/projectDetailData/commaDetail.ts
@@ -12,18 +12,6 @@ export const commaDetail: DetailDataType = {
         "TailwindCSS를 통한 스타일링",
         "Redux를 활용한 Modal 관리",
     ],
-    // learned: [
-    //     {
-    //         title: "TailwindCSS : Utility-First CSS",
-    //         content:
-    //             "Tailwind는 처음엔 적응에 시간이 많이 필요할 거라 생각했지만, CSS에 익숙한 사람이라면 금방 익숙해질 수 있고, 익숙해진 뒤에는 개발 효율이 크게 올라간다는 걸 알 수 있었습니다. 특히 클래스명으로 빠르게 스타일을 적용하고 확인할 수 있으며, 간단한 반응형 디자인과 자유로운 커스터마이징도 큰 강점으로 느껴졌습니다.",
-    //     },
-    //     {
-    //         title: "Redux : 모달 창 상태 관리",
-    //         content:
-    //             "Redux로 모달창 열림 상태와 내용물을 관리했습니다. 초반 설정은 다른 상태 관리 라이브러리보다 복잡했지만 구현을 완료하고 커스텀 훅으로 가져다 쓰다 보니 전역 상태의 중앙집중 관리가 편리한것을 알 수 있었습니다.",
-    //     },
-    // ],
     learned: [
         {
             title: "TailwindCSS",
--- src/data/projectDetailData/meletiDetail.ts
+++ src/data/projectDetailData/meletiDetail.ts
@@ -14,23 +14,6 @@ export const meletiDetail: DetailDataType = {
         "개인 독서 기록 및 단어/문구 공유 커뮤니티 (구현중)",
         "구글, 깃허브 소셜 로그인",
     ],
-    // learned: [
-    //     {
-    //         title: "Next.js 및 SEO",
-    //         content:
-    //             "Next.js의 SSR을 활용하여 SEO를 학습하고 적용했으며 시맨틱 태그와 메타데이터, JSON-LD 등 SEO에 중요한 요소들을 학습했습니다.",
-    //     },
-    //     {
-    //         title: "Three.js를 통한 도서 3D 구현",
-    //         content:
-    //             "책을 구매하기전 입체로 책을 돌려보며 두께나 크기를 짐작할 수 있으면 좋겠다는 생각에서 Three.js를 활용했습니다. 모양과 질감을 정의하고 이미지를 덮어 씌우거나 조명을 통한 그림자를 만드는 Three.js의 기본 사용법을 익힐 수 있었습니다.",
-    //     },
-    //     {
-    //         title: "Tanstack Query와 Firebase firestore",
-    //         content:
-    //             "알라딘 API를 통해 도서데이터를 가져왔고 Tanstack Query로 API 요청을 관리했습니다. 반복작성되는 코드를 많이 줄이고 캐싱 및 리페치 등의 기능들을 상황에 맞게 빠르게 적용할 수 있어 편리했습니다. 유저 데이터는 Firebase의 Firestore DB에서 관리했습니다.",
-    //     },
-    // ],
     learned: [
         {
             title: "3D 도서 구현 및 성능 최적화",
